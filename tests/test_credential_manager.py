651c782c77d9197be9251b42ed7c4f15869c7c5b
Eren Şahin <sahineren.09@gmail.com>
2022-07-04 23:24:15 +0300

    feat: api_url is also retrieved through credential manager

--- tests/test_credential_manager.py
+++ tests/test_credential_manager.py
@@ -64,3 +64,39 @@ class TestCredentialManager(unittest.TestCase):
 
         self.assertIn(
             "'api_key' is required in credential file: ", str(exc.exception))
+
+    @mock.patch("src.credential_manager.json.load")
+    @mock.patch("src.credential_manager.open")
+    def test_get_api_url(self, mock_open, mock_json_load):
+        """
+        test that get_api_url method returns expected api key
+
+        not that open method is mocked even we don't set it a return_value
+        for it. it is because we need to open a file stream to read json file
+        """
+        credentials = {"api_url": "foo"}
+        mock_json_load.return_value = credentials
+
+        cm = CredentialManager()
+        api_url = cm.get_api_url()
+        self.assertEqual("foo", api_url)
+
+    @mock.patch("src.credential_manager.json.load")
+    @mock.patch("src.credential_manager.open")
+    def test_get_api_url_failure(self, mock_open, mock_json_load):
+        """
+        test that get_api_url method fails to find `api_url` and raises
+        `KeyError`
+
+        not that open method is mocked even we don't set it a return_value
+        for it. it is because we need to open a file stream to read json file
+        """
+        credentials = {"apii_url": "foo"}
+        mock_json_load.return_value = credentials
+
+        cm = CredentialManager()
+        with self.assertRaises(KeyError) as exc:
+            cm.get_api_url()
+
+        self.assertIn(
+            "'api_url' is required in credential file: ", str(exc.exception))
